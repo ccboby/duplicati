f9ee0cc74596568d15e067ac985015f619d0937c
ccboby@126.com <ccboby@126.com>
2020-01-10 15:13:22 +0800

    Revert "增加OEM说明文件，更改bat文件"
    This reverts commit ead8873d1fa743096b3487324bd090b6e11c73d9.

--- Installer/Windows/build-msi.bat
+++ Installer/Windows/build-msi.bat
@@ -12,7 +12,6 @@ IF NOT EXIST "%1" (
 call "%VS140COMNTOOLS%vsvars32.bat"
 
 7z x -oDuplicati %1
-xcopy ..\..\Duplicati\Server\webroot\* Duplicati\webroot\ /e /s /y /i
 
 IF EXIST "..\oem" (
 	echo Installing OEM files
